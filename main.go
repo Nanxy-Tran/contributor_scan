f056b66af66473c117018249e283e4438d917456
nanxy.tran <eeit104@gmail.com>
2022-05-22 15:23:04 +0700

    scan for fun

--- main.go
+++ main.go
@@ -4,15 +4,58 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os/exec"
 )
 
+var ignoreFiles = []string{".git", ".idea"}
+
 func main() {
-	files, err := ioutil.ReadDir("logs")
+	scanFolder("./")
+}
+
+func scanFolder(root string) {
+	files, err := ioutil.ReadDir(root)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	for _, file := range files {
-		fmt.Println(file.Name(), file.IsDir())
+		var filePath string
+		if root == "./" {
+			filePath = root + file.Name()
+		} else {
+			filePath = root + "/" + file.Name()
+		}
+
+		if sliceContain(file.Name(), ignoreFiles) {
+			continue
+		}
+
+		if file.IsDir() {
+			scanFolder(filePath)
+			continue
+		}
+
+		checkAuthor(filePath)
 	}
+}
+
+func checkAuthor(filePath string) {
+	cmd := exec.Command("bash", "-c", "git blame "+filePath+" --porcelain | grep '^author '")
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(output), filePath)
+
+}
+
+func sliceContain(searchValue string, slice []string) bool {
+	for _, item := range slice {
+		if item == searchValue {
+			return true
+		}
+	}
+	return false
 }
