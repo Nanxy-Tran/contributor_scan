4044109e78a181a5afd14c715aa2e86fe3554fb5
nanxy.tran <eeit104@gmail.com>
2022-05-22 20:02:13 +0700

    concurrency scan

--- main.go
+++ main.go
@@ -5,16 +5,47 @@ import (
 	"io/ioutil"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 var ignoreFiles = []string{".git", ".idea"}
+var contributions = make(map[string]int)
 
 func main() {
-	scanFolder("./")
+	process := make(chan string)
+	authorChan := make(chan string)
+	go scanFolder("./", process)
+
+	go func() {
+	ProcessAuthor:
+		for {
+			select {
+			case author := <-authorChan:
+				if author == "DONE" {
+					break ProcessAuthor
+				}
+				countContribution(author)
+			}
+		}
+	}()
+
+ProcessFile:
+	for {
+		select {
+		case fileLocation := <-process:
+			checkAuthor(fileLocation, authorChan)
+			if fileLocation == "DONE" {
+				break ProcessFile
+			}
+		}
+	}
+
+	fmt.Println(contributions)
 }
 
-func scanFolder(root string) {
+func scanFolder(root string, process chan string) {
 	files, err := ioutil.ReadDir(root)
+
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -32,22 +63,46 @@ func scanFolder(root string) {
 		}
 
 		if file.IsDir() {
-			scanFolder(filePath)
+			scanFolder(filePath, process)
 			continue
 		}
 
-		checkAuthor(filePath)
+		process <- filePath
 	}
+
+	if root == "./" {
+		process <- "DONE"
+	}
+
+	return
 }
 
-func checkAuthor(filePath string) {
-	cmd := exec.Command("bash", "-c", "git blame "+filePath+" --porcelain | grep '^author '")
+func checkAuthor(filePath string, process chan string) {
+	if filePath == "DONE" {
+		process <- "DONE"
+		return
+	}
+
+	cmd := exec.Command("bash", "-c", "git blame "+filePath+" --porcelain | grep '^author ' | sort -u")
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(output), filePath)
+	process <- parseAuthor(string(output))
+}
+
+func parseAuthor(outputMessage string) string {
+	author := strings.Split(outputMessage, "author ")[1]
+	return author
+}
+
+func countContribution(author string) {
+	if _, ok := contributions[author]; ok {
+		contributions[author]++
+	} else {
+		contributions[author] = 1
+	}
 
 }
 
